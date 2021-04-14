13f185fe765635faacebeabb0e0537759993f322
Samantha Mathistad <ssmathistad@mail.csuchico.edu>
2021-04-14 00:52:52 -0700

    Use latest tag

---
+++ tagBuilder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+NEW_TAG=$1
+
+#only tag if no tag already
+if [ -z "$NEEDS_TAG" ]; then
+    git tag $NEW_TAG
+    echo "Tagged with $NEW_TAG"
+else
+    echo "Already a tag on this commit"
+fi
